6a8efb88d2694490c1ceb559a848e64bab97f4ae
Rahul Pachori <rahulpachori0278@gmail.com>
2026-07-09 20:43:11 +0530

    removed some unnecessary files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,15 +23,10 @@ function App() {
     checkAuth();
   }, [checkAuth]);
 
-  // console.log({authUser});
 
   if(isCheckingAuth && !authUser) return <div className="flex items-center justify-center h-screen">
     <Loader className="size-10 animate-spin"/>
   </div>
-
-  // useEffect(() => {
-  //   document.documentElement.setAttribute("data-theme", theme);
-  // }, [theme]);
 
   return (
     <div data-theme={theme}>
